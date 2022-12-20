ef24ebb9ce6328e048a1a6d7343e0cd5ca80398b
loalmi <114482797+loalmi@users.noreply.github.com>
2022-12-20 05:22:00 +0300

    Add files via upload

---
+++ web_server.py
@@ -0,0 +1,17 @@
+import socket
+sock = socket.socket()
+try:
+  sock.bind(('', 80))
+except OSError:
+  sock.bind(('', 8080))
+sock.listen(5)
+conn, addr = sock.accept()
+print("Connected", addr)
+conn.close()
+
+
+conn, addr = sock.accept()
+print("Connected", addr)
+data = conn.recv(8192)
+msg = data.decode()
+print(msg)
